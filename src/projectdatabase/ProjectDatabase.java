daddbaa79d228bb83732eb0d8783ebf6e6df58c7
JohnsNewHP\John's New HP <jtstegeman@wpi.edu>
2017-03-26 13:44:15 -0400

    Initial Git

---
+++ src/projectdatabase/ProjectDatabase.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectdatabase;
+
+/**
+ *
+ * @author John Stegeman <j.stegeman@labyrinth-tech.com>
+ */
+public class ProjectDatabase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
