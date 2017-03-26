6f40c5643e6eb9a9a080c0397f1fe099cc7b745d
JohnsNewHP\John's New HP <jtstegeman@wpi.edu>
2017-03-26 17:31:45 -0400

    Initial DB access methods.

--- src/com/slackers/inc/database/ProjectDatabase.java
+++ src/com/slackers/inc/database/ProjectDatabase.java
@@ -5,17 +5,48 @@
  */
 package com.slackers.inc.database;
 
+import com.slackers.inc.database.entites.LabelApplication;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  *
  * @author John Stegeman <j.stegeman@labyrinth-tech.com>
  */
-public class ProjectDatabase {
+public class ProjectDatabase{
 
+    
     /**
      * @param args the command line arguments
+     * @throws java.sql.SQLException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        
+        LabelApplication app1 = new LabelApplication("Generic Wine 7", "John Doe");
+        
+        DerbyConnection db = DerbyConnection.getInstance();
+        
+        if (!db.tableExists(app1.getTableName()))
+        {
+            db.createTable(app1.getTableName(), app1.tableColumnCreationSettings());
+        }
+        System.out.println(app1);
+        
+        db.writeEntity(app1, "BeverageName");
+        db.getEntity(app1, "BeverageName");
+        System.out.println(app1);
+        
+        app1.setSubmitter("Rob Smith");
+        db.writeEntity(app1, "BeverageName");
+        db.getEntity(app1, "BeverageName");
+        System.out.println(app1);
+        
+        db.closeConnection();
+        db.shutdownDb();
     }
+    
+    
     
 }
---
+++ src/com/slackers/inc/database/entites/LabelApplication.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.slackers.inc.database.entites;
+
+import java.sql.Date;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author John Stegeman <j.stegeman@labyrinth-tech.com>
+ */
+public class LabelApplication implements IEntity{
+
+    private Date submissionDate;
+    private String beverageName;
+    private String submitter;
+    private int id;
+    
+    public LabelApplication(String beverageName, String submitter, Date submissionDate)
+    {
+        this.beverageName = beverageName;
+        this.submitter = submitter;
+        this.submissionDate = submissionDate;
+        this.id=-1;
+    }
+    public LabelApplication(String beverageName, String submitter)
+    {
+        this(beverageName, submitter, new java.sql.Date(java.sql.Date.from(Instant.now()).getTime()));
+    }
+
+    public Date getSubmissionDate() {
+        return submissionDate;
+    }
+
+    public String getBeverageName() {
+        return beverageName;
+    }
+
+    public String getSubmitter() {
+        return submitter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setSubmissionDate(Date submissionDate) {
+        this.submissionDate = submissionDate;
+    }
+
+    public void setBeverageName(String beverageName) {
+        this.beverageName = beverageName;
+    }
+
+    public void setSubmitter(String submitter) {
+        this.submitter = submitter;
+    }
+    
+    
+    
+    @Override
+    public String getTableName() {
+        return "LabelApplications";
+    }
+
+    @Override
+    public Map<String, Object> getEntityValues() {
+        Map<String,Object> out = new HashMap<>();
+        out.put("BeverageName", this.beverageName);
+        out.put("SubmittedDate", this.submissionDate);
+        out.put("SubmitterName", this.submitter);
+        return out;
+    }
+
+    @Override
+    public void setEntityValues(Map<String, Object> values) {
+        if (values.get("ApplicationId")!=null)
+        {
+            this.id = (Integer)values.get("ApplicationId");
+        }
+        if (values.get("BeverageName")!=null)
+        {
+            this.beverageName = (String)values.get("BeverageName");
+        }
+        if (values.get("SubmittedDate")!=null)
+        {
+            this.submissionDate = (Date)values.get("SubmittedDate");
+        }
+        if (values.get("SubmitterName")!=null)
+        {
+            this.submitter = (String)values.get("SubmitterName");
+        }
+    }
+
+    @Override
+    public Map<String, Class> getEntityNameTypePairs() {
+        Map<String,Class> out = new HashMap<>();
+        out.put("ApplicationId", Integer.class);
+        out.put("BeverageName", String.class);
+        out.put("SubmittedDate", java.sql.Date.class);
+        out.put("SubmitterName", String.class);
+        return out;
+    }
+
+    @Override
+    public List<String> tableColumnCreationSettings() {
+        List<String> cols = new LinkedList<>();
+        cols.add("ApplicationId int PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1)");
+        cols.add("BeverageName varchar(256)");
+        cols.add("SubmittedDate Date");
+        cols.add("SubmitterName varchar(128)");
+        return cols;
+    }
+
+    @Override
+    public String toString() {
+        return "LabelApplication{" + "\n\tsubmissionDate=" + submissionDate + "\n\tbeverageName=" + beverageName + "\n\tsubmitter=" + submitter + "\n\tid=" + id + '}';
+    }
+    
+    
+    
+}
